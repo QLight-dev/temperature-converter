24dc1cdaa66e9c24c2cda8226149b1093261d16f
Muhammed Uzair <nayazmuhammed094@gmail.com>
2026-03-04 08:43:07 +0400

    refactor(test): change got to input

--- main_test.go
+++ main_test.go
@@ -5,11 +5,11 @@ import "testing"
 func TestFahrenheitToCelsius(t *testing.T) {
 	tests := []struct {
 		name string
-		got float64
+		input float64
 		want float64
 	}{
-		{name: "normal case", got: FahrenheitToCelsius(13.0), want: (13.0 - 32.0) * 5/9},
-		{name: "negative", got: FahrenheitToCelsius(-65.0), want: (-65.0 - 32.0) * 5/9},
+		{name: "normal case", input: 13.0, want: (13.0 - 32.0) * 5/9},
+		{name: "negative", input: -65.6, want: (-65.6 - 32.0) * 5/9},
 	}
 
 	for _, tt := range tests {
