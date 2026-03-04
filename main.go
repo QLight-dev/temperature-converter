611f4f024a00f19498da668a8907184308630c8b
Muhammed Uzair <nayazmuhammed094@gmail.com>
2026-03-04 06:45:23 +0000

    feat(main): completed Fahrenheit to Kelvin konversion

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 const (
-	freezingF = 32
+	freezingF = 32.0
 	factor    = 5.0 / 9.0
 )
 
 // converts Fahrenheit temperatures into Celsius
 func FahrenheitToCelsius(temperature float64) float64 {
 	return (temperature - freezingF) * factor
+}
+func FahreheitToKelvin(temperature float64) float64 {
+	return (temperature-32.0)*5/9 + 273.15
 }
--- main_test.go
+++ main_test.go
@@ -4,21 +4,42 @@ import "testing"
 
 func TestFahrenheitToCelsius(t *testing.T) {
 	tests := []struct {
-		name string
+		name  string
 		input float64
-		want float64
+		want  float64
 	}{
-		{name: "normal case", input: 13.0, want: (13.0 - 32.0) * 5/9},
-		{name: "negative", input: -65.6, want: (-65.6 - 32.0) * 5/9},
+		{name: "normal case", input: 13.0, want: (13.0 - 32.0) * 5 / 9},
+		{name: "negative", input: -65.6, want: (-65.6 - 32.0) * 5 / 9},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			got := FahrenheitToCelsius(test.input)
-			if got != want {
+			if got != test.want {
 				t.Errorf("input: %v, got: %v, want: %v", test.input, got, test.want)
 			}
 		})
 	}
 }
 
+func TestFahreheitToKelvin(t *testing.T) {
+	tests := []struct {
+		name  string
+		input float64
+		want  float64
+	}{
+		{name: "normal case", input: 98.5, want: (98.5-32.0)*5/9 + 273.15},
+		{name: "negative", input: -75.6, want: (-75.6-32.0)*5/9 + 273.15},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got := FahreheitToKelvin(test.input)
+			// floating-point arithmetic won't be exact, so we check if its close enough to the expected value
+			delta := 0.0001
+			if got < test.want-delta || got > test.want+delta {
+				t.Errorf("input: %v, got: %v, want: %v", test.input, got, test.want)
+			}
+		})
+	}
+}
